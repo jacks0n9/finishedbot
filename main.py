ad5f57fbc30a16dfc98787a0b809abb61f7b63aa
milodogexists <elnepik@outlook.com>
2021-01-26 10:53:18 -0800

    Better handling of errors

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import random
 import os
 import praw
+
 sendSlackAlerts = True
 mybot=praw.Reddit("bot1")
 subreddit=mybot.subreddit("freekarma4u")
@@ -25,23 +26,25 @@ def docomment():
 			with open("posts_replied_to.txt","a") as posts_replied_to:
 				posts_replied_to.write(submission.id+",")
 			done=open("posts_replied_to.txt",'r').read().split(',')
-try:
-	docomment()
-except KeyboardInterrupt:
-	print("\nOk stopping bot")
-	exit(0)
-except Exception as error:
-	if not os.path.isfile("logs.txt"):
-		open("logs.txt", 'w').write('')
-	now = datetime.now()
-	date = now.strftime("%m/%d/%Y %H:%M:%S")
-	file = open("logs.txt",'a')
-	file.write("\n"+date)
-	file.write("\n"+str(error))
-	print("Unknown error, find the error in logs.txt")
-	if sendSlackAlerts:
-		slacktoken = open("slacktoken.txt").read()
-		client = slack.WebClient(token=slacktoken)
-		client.chat_postMessage(channel='alerts', text=f"An error has occured:\n{error}")
+def go():
+	try:
+		docomment()
+	except KeyboardInterrupt:
+		print("\nOk stopping bot")
+		exit(0)
+	except Exception as error:
+		if not os.path.isfile("logs.txt"):
+			open("logs.txt", 'w').write('')
+		now = datetime.now()
+		date = now.strftime("%m/%d/%Y %H:%M:%S")
+		file = open("logs.txt",'a')
+		file.write("\n"+date)
+		file.write("\n"+str(error))
+		print("Unknown error, find the error in logs.txt")
+		if sendSlackAlerts:
+			slacktoken = open("slacktoken.txt").read()
+			client = slack.WebClient(token=slacktoken)
+			client.chat_postMessage(channel='alerts', text=f"An error has occured:\n{error}")
+		go()
 
-
+go()
